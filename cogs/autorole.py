a2b78250739dc354f1ee0aaeb46ef46dc6b60568
TheAdryanBr <adryan_s_santana@hotmail.com>
2025-12-03 22:34:30 -0300

    Update autorole.py

--- cogs/autorole.py
+++ cogs/autorole.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 class AutoRole(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.role_id = CARGO_ID_AQUI  # coloque o ID do cargo
+        self.role_id = 1213324989202309220  # coloque o ID do cargo
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
